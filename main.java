d0f5c4f86ea5fcc4f64d71ea4689e4ae4fedca7e
kys <mateopotato08@gmail.com>
2025-05-16 14:46:50 -0700

    Timer works basically

--- main.java
+++ main.java
@@ -1,33 +1,32 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
+//import java.util.*;
 import javax.swing.*;
 
 public class main
 {
   public static final Point screen = new Point((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
-  public static double Timer = -1;
+  public static double GameTime = -1;
+  public static int WindowCount;
 
   public static void main(String args[])
   {
-    int WindowCount;
+    int ThisWindowCount;
 
     while (true)
     {
       // Game start
       WaitForClose(MainWindow("Bop It"));
-      WindowCount = 10;
-      while (WindowCount > 0)
+      WindowCount = (int)(Math.random() * 10 + 6);
+      ThisWindowCount = WindowCount;
+      GameTime = System.currentTimeMillis();
+      while (ThisWindowCount > 0)
       {
         // Game loop
-        WaitForClose(ActionWindow("/* " + WindowCount + " */"));
-        WindowCount--;
+        WaitForClose(ActionWindow("/* " + ThisWindowCount + " */"));
+        ThisWindowCount--;
       }
-
-      // Game end
-      boolean playAgain = false;
-      if (playAgain)
-        break;
+      GameTime = (System.currentTimeMillis() - GameTime) / 1000;
     }
 
     /*
@@ -56,7 +55,7 @@ public class main
     final Point W = new Point(600, 400);
     final Point P = new Point((main.screen.x - W.x) / 2, (main.screen.y - W.y) / 2);
     JFrame R = new JFrame(name);
-    R.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    R.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     R.setSize(W.x, W.y);
     R.setResizable(false);
     
@@ -69,33 +68,58 @@ public class main
       }
     });
 
-    R.addWindowListener(new WindowListener()
-    {
-      @Override public void windowClosing(WindowEvent e)
-      {
-        e.getWindow().setEnabled(false);
-      }
-      @Override public void windowOpened(WindowEvent e) {}
-      @Override public void windowClosed(WindowEvent e) {}
-      @Override public void windowIconified(WindowEvent e) {}
-      @Override public void windowDeiconified(WindowEvent e) {}
-      @Override public void windowActivated(WindowEvent e) {}
-      @Override public void windowDeactivated(WindowEvent e) {}
-    });
-
-    if (main.Timer < 0)
+    if (main.GameTime < 0)
     {
       JLabel L = new JLabel("Ready to Bop It?");
-      L.setFont(L.getFont().deriveFont(20f));
+      L.setFont(L.getFont().deriveFont(30f));
       L.setHorizontalAlignment(SwingConstants.CENTER);
       R.add(L, BorderLayout.CENTER);
+
+      JButton B = new JButton("BEGIN");
+      B.setPreferredSize(new Dimension(100, 50));
+      B.setFont(B.getFont().deriveFont(20f));
+      B.addActionListener(new ActionListener()
+      {
+        @Override public void actionPerformed(ActionEvent e)
+        {
+          R.setEnabled(false);
+          R.dispose();
+        }
+      });
+      R.add(B, BorderLayout.SOUTH);
     }
     else
     {
-      JLabel L = new JLabel("Best time: " + main.Timer);
-      L.setFont(L.getFont().deriveFont(20f));
+      JLabel L = new JLabel("<html>Total: " + (main.GameTime) + " seconds<br>Average: " + (main.GameTime / main.WindowCount) + " per window</html>");
+      L.setFont(L.getFont().deriveFont(30f));
       L.setHorizontalAlignment(SwingConstants.CENTER);
       R.add(L, BorderLayout.CENTER);
+      
+      JButton B = new JButton("AGAIN");
+      B.setPreferredSize(new Dimension(100, 50));
+      B.setFont(B.getFont().deriveFont(20f));
+      B.addActionListener(new ActionListener()
+      {
+        @Override public void actionPerformed(ActionEvent e)
+        {
+          R.setEnabled(false);
+          R.dispose();
+        }
+      });
+      R.add(B, BorderLayout.SOUTH);
+      /*
+      JButton C = new JButton("Exit");
+      C.setPreferredSize(new Dimension(100, 50));
+      C.setFont(C.getFont().deriveFont(20f));
+      C.addActionListener(new ActionListener()
+      {
+        @Override public void actionPerformed(ActionEvent e)
+        {
+          System.exit(0);
+        }
+      });
+      R.add(C, BorderLayout.SOUTH);
+      */
     }
 
     R.setVisible(true);
@@ -134,6 +158,7 @@ public class main
       @Override public void windowDeactivated(WindowEvent e) {}
     });
     
+    //R.add(TabbedButton("Bop It", "False Sun"));
     R.setVisible(true);
     return R;
   }
@@ -154,6 +179,25 @@ public class main
   {
     while (frame.isEnabled())
       Wait(0.1);
+  }
+
+  private static Component TabbedButton(String name, String text)
+  {
+    JButton B = new JButton(text);
+    B.setPreferredSize(new Dimension(100, 50));
+    B.setFont(B.getFont().deriveFont(20f));
+    B.addActionListener(new ActionListener()
+    {
+      @Override public void actionPerformed(ActionEvent e)
+      {
+        JFrame R = new JFrame(name);
+        R.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        R.setSize(200, 200);
+        R.setLocationRelativeTo(null);
+        R.setVisible(true);
+      }
+    });
+    return B;
   }
 
   static class CirclePanel extends JPanel
